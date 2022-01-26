a37c026d5ad22b0b00933adac86ad06fc0d10607
Lianming Du <adullb@qq.com>
2022-01-26 21:21:13 +0800

    bump to 0.2.0

--- src/config.py
+++ src/config.py
@@ -7,9 +7,9 @@ from PySide6.QtCore import __version__ as pyside_version
 
 __all__ = ['DOCKEY_VERSION', 'DOCKEY_BUILD', 'DOCKEY_ABOUT']
 
-DOCKEY_VERSION = "0.1.1"
+DOCKEY_VERSION = "0.2.0"
 
-DOCKEY_BUILD = "220113"
+DOCKEY_BUILD = "220126"
 
 DOCKEY_ABOUT = """
 <p>Dockey - Molecular Docking and Virtual Screening</p>
