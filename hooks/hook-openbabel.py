606cc30b30182c156755cd73b77257dc1c84a54c
Lianming Du <adullb@qq.com>
2022-05-25 17:39:00 +0800

    Update hook-openbabel.py

--- hooks/hook-openbabel.py
+++ hooks/hook-openbabel.py
@@ -1,5 +1,5 @@
-from PyInstaller.compat import is_win
+from PyInstaller.compat import is_linux
 from PyInstaller.utils.hooks import collect_all
 
-if is_win:
+if not is_linux:
 	datas, binaries, _ = collect_all('openbabel')
