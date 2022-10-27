c1351308783c56d3e6c30d70157935c633a0ca88
Lianming Du <adullb@qq.com>
2022-10-27 23:02:23 +0800

    fixed ob on windows

--- hooks/hook-openbabel.py
+++ hooks/hook-openbabel.py
@@ -2,15 +2,33 @@ from PyInstaller.compat import is_win, is_linux, is_darwin
 from PyInstaller.utils.hooks import collect_data_files, collect_dynamic_libs
 
 if is_win:
-	datas = collect_data_files('openbabel', subdir='data')
-	binaries = collect_data_files('openbabel', excludes=['data/*', '*.exe'])
+	datas = collect_data_files('openbabel',
+		subdir='data'
+	)
+	binaries = collect_data_files('openbabel',
+		excludes=['__pycache__/*', 'data/*', '*.exe', '*.py'],
+		include_py_files=True
+	)
 
 if is_linux:
-	datas = collect_data_files('openbabel', subdir='data')
-	binaries = collect_data_files('openbabel', subdir='plugin', include_py_files=True)
-	binaries += collect_data_files('openbabel', subdir='../openbabel.libs', include_py_files=True)
+	datas = collect_data_files('openbabel',
+		subdir='data'
+	)
+	binaries = collect_data_files('openbabel',
+		subdir='plugin',
+		include_py_files=True
+	)
+	binaries += collect_data_files('openbabel',
+		subdir='../openbabel.libs',
+		include_py_files=True
+	)
 
 if is_darwin:
-	datas = collect_data_files('openbabel', subdir='data')
-	binaries = collect_data_files('openbabel', subdir='plugin', include_py_files=True)
+	datas = collect_data_files('openbabel',
+		subdir='data'
+	)
+	binaries = collect_data_files('openbabel',
+		subdir='plugin',
+		include_py_files=True
+	)
 	binaries += collect_dynamic_libs('openbabel')
