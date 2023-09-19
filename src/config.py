bf4124e4ee35d2874d47f9855c05f5a8a5c1c9f5
Lianming Du <adullb@qq.com>
2023-09-19 11:17:09 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -16,9 +16,9 @@ from plip.basic.config import __version__ as plip_version
 __all__ = ['DOCKEY_VERSION', 'DOCKEY_BUILD', 'DOCKEY_ABOUT',
 			'DOCKEY_THANKS']
 
-DOCKEY_VERSION = "0.9.3"
+DOCKEY_VERSION = "0.9.4"
 
-DOCKEY_BUILD = "230918"
+DOCKEY_BUILD = "230919"
 
 DOCKEY_ABOUT = """
 <p>Dockey - Molecular Docking and Virtual Screening</p>
