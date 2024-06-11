10f19687bb97ebcac69b616c8de1623fa6bc5224
Lianming Du <adullb@qq.com>
2024-06-11 22:49:55 +0800

    Update pyi_rth_openbabel.py

--- hooks/pyi_rth_openbabel.py
+++ hooks/pyi_rth_openbabel.py
@@ -1,7 +1,11 @@
+import os
 import sys
 
 d = sys._MEIPASS
 
-if not sys.platform.startswith('win32'):
+if sys.platform.startswith('win32'):
+	os.environ['BABEL_DATADIR'] = os.path.join(d, 'openbabel', 'data')
+	os.environ['BABEL_LIBDIR'] = os.path.join(d, 'openbabel')
+else:
 	os.environ['BABEL_DATADIR'] = os.path.join(d, 'openbabel', 'data')
 	os.environ['BABEL_LIBDIR'] = os.path.join(d, 'openbabel', 'lib')
